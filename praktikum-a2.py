c0ad4df27381383cb683013cd19924220e362d6c
kelsianelsa2909 <153788120+kelsianelsa2909@users.noreply.github.com>
2023-12-21 05:51:50 -0800

    Add files via upload

---
+++ praktikum-a2.py
@@ -0,0 +1,230 @@
+print('praktikum-a2\n')
+
+print('nama  : kelsia nelsa')
+print('nim   : 231031009')
+print('prodi : sistem informasi A')
+
+# INI OPERATOR ASSIGMENT
+a = 19
+print('Nilai a adalah',a)
+a += 3
+print('Nilai a+3 sekarang adalah',a)
+
+a = 19
+print('nilai a adalah',a)
+a -=3
+print('nilai a-=3 adala',a)
+
+a = 19
+print('nilai a adalah',a)
+a *= 2
+print('nilai a*=2 adala',a)
+
+a = 19
+print('nilai a adalah',a)
+a /= 2
+print('nilai a/=2 adala',a)
+
+a = 3
+print('nilai a adalah',a)
+a **= 2
+print('nilai a**=2 adala',a)
+
+a = 35
+print('nilai a adalah',a)
+a %= 32
+print('nilai a%=32 adala',a)
+
+a = 35
+print('nilai a adalah',a)
+a //= 32
+print('nilai a//=32 adala',a)
+
+# Tugas melanjutkan untuk operator selanjutnya  line 25-line 59
+
+#OPERATOR PERBANDINGAN
+a = 9
+b = 13
+print('\n--------- Besar dari 13')
+hasil = a > 13 
+print(a,'> 13 adalah',hasil)
+hasil = b > 13
+print(b,'>13 adalah',hasil)
+
+print('\n--------- Besar dari 13')
+hasil = a < 13
+print(a,'< 13 adalah',hasil)
+hasil = b < 13
+print(b,'< 13 adalah',hasil)
+
+
+a = 9
+b = 13
+print('\n--------- Besar dari 9 ')
+hasil = a > 9
+print(a,'> 13 adalah',hasil)
+hasil = b > 9 
+print(b,'>13 adalah',hasil)
+
+print('\n--------- Besar dari 9 ')
+hasil = a < 9
+print(a,'< 13 adalah',hasil)
+hasil = b < 9 
+print(b,'< 13 adalah',hasil)
+
+a=4
+b=9
+print('\n--------- Besar atau sama dari 9 ')
+hasil = a < 9
+print(a,'< 13 adalah',hasil)
+hasil = b < 9 
+print(b,'< 13 adalah',hasil)
+
+
+a=7
+b=9
+print('\n--------- kecil atau sama dari 9 ')
+hasil = a < 9
+print(a,'< 13 adalah',hasil)
+hasil = b < 9 
+print(b,'< 13 adalah',hasil)
+
+a=3
+b=9
+print('\n---------  sama dari 9 ')
+hasil = a < 9
+print(a,'< 13 adalah',hasil)
+hasil = b < 9 
+print(b,'< 13 adalah',hasil)
+
+
+a=2
+b=9
+print('\n--------- tidak  sama dari 9 ')
+hasil = a < 9
+print(a,'< 13 adalah',hasil)
+hasil = b < 9 
+print(b,'< 13 adalah',hasil)
+
+
+
+# OPERATOR LOGIKA
+a = True
+b = False
+print('/n==========AND=========')
+hasil = a and a
+print(a,'and',a,'hasilnya=',hasil)
+hasil = a and b
+print(a,'and',b,'hasilnya=',hasil)
+hasil = b and a
+print(b,'and',a,'hasilnya=',hasil)
+hasil = b and b
+print(b,'and',b,'hasilnya=',hasil)
+
+
+
+print('/n=======OR=======')
+hasil = a or a
+print(a,'or',a,'hasilnya=',hasil)
+hasil = a or b
+print(a,'or',b,'hasilnya=',hasil)
+hasil = b or a
+print(b,'or',a,'hasilnya=',hasil)
+hasil = b or b
+print(b,'or',b,'hasilnya=',hasil)
+
+
+
+
+print('/n==========XOR=========')
+hasil = a ^ a
+print(a,'XOR',a,'hasilnya=',hasil)
+hasil = a ^ b
+print(a,'XOR',b,'hasilnya=',hasil)
+hasil = b ^ a
+print(b,'XOR',a,'hasilnya=',hasil)
+hasil = b ^ b
+print(b,'XOR',b,'hasilnya=',hasil)
+
+
+print('/n==========NOT=========')    
+hasil = not a
+print('NOT',a,'hasilnya=',hasil)
+hasil = not b
+print('NOT',b,'hasilnya=',hasil)
+
+
+
+
+# OPERATOR MEMBERSHIP
+print('/n ================IN')
+nama = 'kelsia nelsa'
+
+test = 'ke'
+cek = test in nama
+print(test,'terdapat di',nama,'adalah', cek)
+
+test = 'ke'
+cek = test in nama
+print(test,'terdapat di',nama,'adalah', cek)
+print()
+test1 = 'a'
+test2 = 'i'
+test3 = 'u'
+test4 = 'e'
+test5 = 'o'
+
+
+cek= test in nama
+print(test,'terdapat di',nama,'adalah', cek)
+
+
+cek= test in nama
+print(test,'terdapat di',nama,'adalah', cek)
+
+
+cek= test in nama
+print(test,'terdapat di',nama,'adalah', cek)
+
+
+cek= test in nama
+print(test,'terdapat di',nama,'adalah', cek)
+
+
+cek= test in nama
+print(test,'terdapat di',nama,'adalah', cek)
+
+
+
+
+
+      
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
