b57c4b213b92fb19e48bc4f9e23aa46d522cb3ca
kelsianelsa2909 <153788120+kelsianelsa2909@users.noreply.github.com>
2023-12-21 05:48:35 -0800

    Add files via upload

---
+++ A Tugas1. 231031009 ganjil.py
@@ -0,0 +1,10 @@
+print(f'Tugas 1 : buat program')
+print()
+
+x = int(input('masukkan Nilai: '))
+if x % 2 != 0 :
+    print("bilangan x adalah bilangan ganjil")
+else:
+    print("bilangan x adalah bukan ganjil")
+         
+        
