dd415377f658087ca487c93eaa65a34131b4fb35
kelsianelsa2909 <153788120+kelsianelsa2909@users.noreply.github.com>
2023-12-21 05:49:15 -0800

    Add files via upload

---
+++ kelsia nelsa 231031009 latihan1.py
@@ -0,0 +1,9 @@
+
+#percabangan if
+nilai = 70
+nilai = float(input('masukkan nilai: '))
+batas_lulus = 65
+if nilai > batas_lulus:
+    print('selamat,kamu lulus!')
+else:
+    print('sayang sekali,kamu tidak lulus!')
