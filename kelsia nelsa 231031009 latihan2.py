6cb7e1c1ff079aa68cad1d6151d846c1dee20d68
kelsianelsa2909 <153788120+kelsianelsa2909@users.noreply.github.com>
2023-12-21 05:49:32 -0800

    Add files via upload

---
+++ kelsia nelsa 231031009 latihan2.py
@@ -0,0 +1,7 @@
+#Menerima input dari pengguna
+inp = input('Masukkan Suatu input: ')
+#Mengecek panjang input
+if len(inp) !=3:
+    print('Panjang string ', len(inp),'tidak sama dengan 3')
+else:
+ print('Panjang input sesuai yang diinginkan')
