a7c628399e337ffb8dd1655bddd8811ad66b1e50
kelsianelsa2909 <153788120+kelsianelsa2909@users.noreply.github.com>
2023-12-21 05:54:01 -0800

    Add files via upload

---
+++ tugas 5 kelsia nelsa-231031009-praktikum-py5.py
@@ -0,0 +1,13 @@
+# mendefinisikan variabel a dengan nilai boolean True.
+a = True
+count = 0
+
+#while a::memulai loop while yang akan berjalan selama  nilai variabel a adalah True.
+while a:
+        print('Menjalankan Program')
+        count += 1
+        if count == 5:
+            break
+
+#`count += 1`: Menambah penghitung sebanyak 1 di setiap iterasi.
+# `jika hitungan == 5: istirahat`: Memeriksa apakah penghitung telah mencapai 5 dan keluar dari perulangan jika benar.
